b9c03b4d1f993de3607eeb447a67c1927d839915
zhiqin.zhang <zhiqin.zhang@wehotelgloabl.com>
2019-06-25 18:08:50 +0800

    添加hashtable he vector 实例

---
+++ src/main/java/com/currency/example/syncContainer/HashTableExample1.java
@@ -0,0 +1,55 @@
+package com.currency.example.syncContainer;
+
+import com.currency.annoations.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+@Slf4j
+@ThreadSafe
+public class HashTableExample1 {
+
+    private static int clientTotal = 5000;
+    private static int threadTotal = 200;
+    private static int count = 0;
+    private static Map map = new Hashtable();
+
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal; i++) {
+            final int count = i;
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    add(count);
+                    semaphore.release();
+                } catch (Exception e) {
+                    log.error("exception:", e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        log.info("count:{}", map.size());
+        executorService.shutdown();
+    }
+
+    /**
+     * 此处synchronized 去掉为线程不安全
+     */
+
+    private synchronized static void add(int i) {
+        map.put(i, i);
+    }
+
+}
---
+++ src/main/java/com/currency/example/syncContainer/VectorExample2.java
@@ -0,0 +1,32 @@
+package com.currency.example.syncContainer;
+
+import com.currency.annoations.NotThreadSafe;
+
+import java.util.Vector;
+
+@NotThreadSafe
+public class VectorExample2 {
+
+    private static final Vector<Integer> vector = new Vector<Integer>();
+
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            vector.add(i);
+        }
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                vector.remove(i);
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                vector.get(i);
+            }
+        });
+        thread1.start();
+        thread2.start();
+    }
+
+}
